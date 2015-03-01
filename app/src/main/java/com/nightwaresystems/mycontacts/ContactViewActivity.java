ceb9100bad4dc7abbabae67c947da3cfc4e9bfef
mhhonline <mhhonline@gmail.com>
2015-03-01 22:48:33 +0300

    Update ContactViewActivity.java

--- app/src/main/java/com/nightwaresystems/mycontacts/ContactViewActivity.java
+++ app/src/main/java/com/nightwaresystems/mycontacts/ContactViewActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
 
-public class ContactViewActivity extends ActionBarActivity {
+public class ContactViewActivity extends ActionBarActivity
+{
 
     public static final String EXTRA = ContactViewActivity.EXTRA;
 
@@ -14,12 +15,12 @@ public class ContactViewActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_view);
 
-        if(getFragmentManager().findFragmentById(R.id.view_fragment_container) == null)
-        {
+        if (getFragmentManager().findFragmentById(R.id.view_fragment_container) == null) {
+            ContactViewFragment cvf = new ContactViewFragment();
+            cvf.setArguments(getIntent().getExtras());
             getFragmentManager().beginTransaction()
-                    .add(R.id.view_fragment_container,new ContactListFragment())
+                    .add(R.id.view_fragment_container, cvf)
                     .commit();
         }
-
     }
 }
